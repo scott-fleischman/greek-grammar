14a2321a0857cd8a620fbe94ee6d0341090a093f
Scott Fleischman <scott.fleischman@logos.com>
2015-02-06 09:06:23 -0800

    Read more structure from SBLGNT.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -3,22 +3,49 @@
 
 module Main where
 
-import BasicPrelude hiding (readFile)
+import BasicPrelude hiding (readFile, Word)
 import Data.Set (fromList, toList)
 import Data.Text (pack)
 import Text.XML
 import Text.XML.Cursor
 
-osisElement :: Text -> Cursor -> [Cursor]
-osisElement localName = element $ Name localName (Just "http://www.bibletechnologies.net/2003/OSIS/namespace") Nothing
+data Reference = Reference
+  { getResource :: Text
+  , getLocation :: Text
+  } deriving (Show)
 
-sblgntWords :: Document -> [Text]
-sblgntWords d = fromDocument d $// osisElement "w" &// content
+data Word = Word
+  { getWord :: Text
+--  , reference :: Reference
+  } deriving (Show)
 
-distinctWords :: [Text] -> [Text]
-distinctWords ws = toList $ fromList ws
+data Resource = Resource
+  { getResourceId :: Text
+  , getWords :: [Word]
+  } deriving (Show)
+
+osisName :: Text -> Name
+osisName localName = Name localName (Just "http://www.bibletechnologies.net/2003/OSIS/namespace") Nothing
+
+osisElement :: Text -> Axis
+osisElement = element . osisName
+
+osisAttribute :: Text -> Cursor -> [Text]
+osisAttribute = attribute . osisName
+
+loadSblgnt :: Document -> Resource
+loadSblgnt d = Resource resourceId words
+  where
+    cursor = fromDocument d
+    resourceId = concat $ concatMap (attribute "osisIDWork") $ cursor $/ osisElement "osisText"
+    words = fmap Word $ cursor $// osisElement "w" &/ content
+
+distinctBySet :: Ord a => [a] -> [a]
+distinctBySet ws = toList $ fromList ws
 
 main :: IO ()
 main = do
-  sblgntDocument <- readFile def $ ".." </> "sblgnt" </> "osis" </> "SBLGNT" <.> "osis" <.> "xml"
-  mapM_ putStrLn $ distinctWords $ sblgntWords sblgntDocument
+  sblgntDocument <- readFile def $ ".." </> "sblgnt" </> "osis" </> "SBLGNT" <.> "osis" <.> "xml" -- http://sblgnt.com/
+  let r = loadSblgnt sblgntDocument
+  putStrLn $ getResourceId r
+  mapM_ putStrLn $ distinctBySet $ fmap getWord $ getWords r
