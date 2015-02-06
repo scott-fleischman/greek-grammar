751b225736f0fe382028dbf2e675efd7bd8e4dc6
Scott Fleischman <scott.fleischman@logos.com>
2015-02-06 00:05:57 -0800

    Print unique SBLGNT words.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import BasicPrelude hiding (readFile)
+import Data.Set (fromList, toList)
+import Data.Text (pack)
+import Text.XML
+import Text.XML.Cursor
+
+osisElement :: Text -> Cursor -> [Cursor]
+osisElement localName = element $ Name localName (Just "http://www.bibletechnologies.net/2003/OSIS/namespace") Nothing
+
+sblgntWords :: Document -> [Text]
+sblgntWords d = fromDocument d $// osisElement "w" &// content
+
+distinctWords :: [Text] -> [Text]
+distinctWords ws = toList $ fromList ws
+
 main :: IO ()
-main = putStrLn "χαῖρε"
+main = do
+  sblgntDocument <- readFile def $ ".." </> "sblgnt" </> "osis" </> "SBLGNT" <.> "osis" <.> "xml"
+  mapM_ putStrLn $ distinctWords $ sblgntWords sblgntDocument
