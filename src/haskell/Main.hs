3755b9c4e12bd8f40a09aaf7af8d49f077a3b2a7
Scott Fleischman <scott.fleischman@logos.com>
2015-02-09 13:22:01 -0800

    Add reference context to sblgnt parsing.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -3,49 +3,64 @@
 
 module Main where
 
-import BasicPrelude hiding (readFile, Word)
+import BasicPrelude hiding (readFile, Word, lookup)
+import Data.Char (ord)
+import Data.Map (lookup)
 import Data.Set (fromList, toList)
-import Data.Text (pack)
+import Data.Text (pack, unpack)
+import Numeric (showHex)
 import Text.XML
 import Text.XML.Cursor
 
-data Reference = Reference
-  { getResource :: Text
-  , getLocation :: Text
+data BibleVerse = BibleVerse
+  { bibleVerse :: Text
   } deriving (Show)
 
 data Word = Word
-  { getWord :: Text
---  , reference :: Reference
+  { wordText :: Text
+  , wordBibleVerse :: BibleVerse
   } deriving (Show)
 
 data Resource = Resource
-  { getResourceId :: Text
-  , getWords :: [Word]
+  { resourceName :: Text
+  , resourceWords :: [Word]
   } deriving (Show)
 
+osisNamespace :: Text
+osisNamespace = "http://www.bibletechnologies.net/2003/OSIS/namespace"
+
 osisName :: Text -> Name
-osisName localName = Name localName (Just "http://www.bibletechnologies.net/2003/OSIS/namespace") Nothing
+osisName localName = Name localName (Just osisNamespace) Nothing
 
 osisElement :: Text -> Axis
 osisElement = element . osisName
 
-osisAttribute :: Text -> Cursor -> [Text]
-osisAttribute = attribute . osisName
+loadSblgntWords :: Cursor -> [Word]
+loadSblgntWords cursor = reverse $ snd $ foldl' buildWords (BibleVerse "", []) elements
+  where
+    buildWords = \ state@(r, ws) c -> case node c of
+      n@(NodeElement e) -> case nameLocalName $ elementName e of
+        "verse" -> case lookup "sID" (elementAttributes e) of
+          Just v -> (BibleVerse v, ws)
+          _ -> state
+        "w" -> (r, Word (concat $ fromNode n $// content) r : ws)
+        _ -> state
+      _ -> state
+    elements = cursor $// osisElement "div" >=> attributeIs "type" "book" &/ child
 
 loadSblgnt :: Document -> Resource
 loadSblgnt d = Resource resourceId words
   where
     cursor = fromDocument d
     resourceId = concat $ concatMap (attribute "osisIDWork") $ cursor $/ osisElement "osisText"
-    words = fmap Word $ cursor $// osisElement "w" &/ content
+    words = loadSblgntWords cursor
 
 distinctBySet :: Ord a => [a] -> [a]
-distinctBySet ws = toList $ fromList ws
+distinctBySet = toList . fromList
 
 main :: IO ()
 main = do
   sblgntDocument <- readFile def $ ".." </> "sblgnt" </> "osis" </> "SBLGNT" <.> "osis" <.> "xml" -- http://sblgnt.com/
   let r = loadSblgnt sblgntDocument
-  putStrLn $ getResourceId r
-  mapM_ putStrLn $ distinctBySet $ fmap getWord $ getWords r
+  putStrLn $ resourceName r
+  mapM_ (putStrLn) $ fmap (\w -> concat [bibleVerse . wordBibleVerse $ w, ": ", wordText w]) $ resourceWords r
