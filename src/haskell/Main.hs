a78c3e73da0d80dd6c05d0fa69757cae637a5e0e
Scott Fleischman <scott.fleischman@logos.com>
2015-02-10 00:56:01 -0800

    Add paragraph context.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -16,37 +16,66 @@ data BibleVerse = BibleVerse
   { bibleVerse :: Text
   } deriving (Show)
 
+newtype ParagraphIndex = ParagraphIndex { paragraphIndex :: Word32 }
+  deriving (Eq, Ord, Show)
+
 data Word = Word
   { wordText :: Text
   , wordBibleVerse :: BibleVerse
+  , wordParagraphIndex :: ParagraphIndex
+  } deriving (Show)
+
+data WordBuilderError =
+  TextWithoutVerse Text |
+  SkippedElement Text |
+  SkippedNode Node
+  deriving (Show)
+
+data Builder = Builder
+  { builderBibleVerse :: Maybe BibleVerse
+  , builderParagraphIndex :: ParagraphIndex
+  , builderWords :: [Word]
+  , builderErrors :: [WordBuilderError]
   } deriving (Show)
 
 data Resource = Resource
   { resourceName :: Text
-  , resourceWords :: [Word]
+  , resourceBuilder :: Builder
   } deriving (Show)
 
 osisNamespace :: Text
 osisNamespace = "http://www.bibletechnologies.net/2003/OSIS/namespace"
 
 osisName :: Text -> Name
-osisName localName = Name localName (Just osisNamespace) Nothing
+osisName n = Name n (Just osisNamespace) Nothing
 
 osisElement :: Text -> Axis
 osisElement = element . osisName
 
-loadSblgntWords :: Cursor -> [Word]
-loadSblgntWords cursor = reverse $ snd $ foldl' buildWords (BibleVerse "", []) elements
-  where
-    buildWords = \ state@(r, ws) c -> case node c of
-      n@(NodeElement e) -> case nameLocalName $ elementName e of
-        "verse" -> case lookup "sID" (elementAttributes e) of
-          Just v -> (BibleVerse v, ws)
-          _ -> state
-        "w" -> (r, Word (concat $ fromNode n $// content) r : ws)
-        _ -> state
-      _ -> state
-    elements = cursor $// osisElement "div" >=> attributeIs "type" "book" &/ child
+emptyBuilder :: Builder
+emptyBuilder = Builder Nothing (ParagraphIndex 0) [] []
+
+buildWords :: Builder -> Cursor -> Builder
+buildWords builder@(Builder mv p ws es) c =
+  case node c of
+    n@(NodeElement e) ->
+      case nameLocalName $ elementName e of
+        "verse" ->
+          case (lookup "sID" (elementAttributes e), lookup "eID" (elementAttributes e)) of
+            (Just v, _) -> builder { builderBibleVerse = Just $ BibleVerse v }
+            (_, Just _) -> builder { builderBibleVerse = Nothing }
+            _ -> builder
+        "w" -> let t = (concat $ fromNode n $// content) in
+          case mv of
+            Just v -> builder { builderWords = w : ws } where w = Word t v p
+            _ -> builder { builderErrors = (TextWithoutVerse t) : es }
+        "p" -> builder { builderParagraphIndex = ParagraphIndex (1 + (paragraphIndex p)) }
+        _ -> builder
+    _ -> builder
+
+loadSblgntWords :: Cursor -> Builder
+loadSblgntWords cursor = foldl' buildWords emptyBuilder elements
+  where elements = cursor $// osisElement "div" >=> attributeIs "type" "book" &.// descendant
 
 loadSblgnt :: Document -> Resource
 loadSblgnt d = Resource resourceId words
@@ -58,9 +87,12 @@ loadSblgnt d = Resource resourceId words
 distinctBySet :: Ord a => [a] -> [a]
 distinctBySet = toList . fromList
 
+showWords :: Builder -> [Text]
+showWords = fmap (\(Word t v i) -> concat [bibleVerse v, " [p=", show . paragraphIndex $ i, "] ", t] ) . reverse . builderWords
+
 main :: IO ()
 main = do
   sblgntDocument <- readFile def $ ".." </> "sblgnt" </> "osis" </> "SBLGNT" <.> "osis" <.> "xml" -- http://sblgnt.com/
   let r = loadSblgnt sblgntDocument
   putStrLn $ resourceName r
-  mapM_ (putStrLn) $ fmap (\w -> concat [bibleVerse . wordBibleVerse $ w, ": ", wordText w]) $ resourceWords r
+  mapM_ putStrLn $ showWords . resourceBuilder $ r
