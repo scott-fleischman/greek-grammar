797df8dc75f207ca95c8b64b49540ee70004b7df
Scott Fleischman <scott.fleischman@logos.com>
2015-02-23 13:09:22 -0800

    Use text-format for padding output columns.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -7,42 +7,54 @@ import Control.Applicative ((<$>))
 import Control.Monad (mapM_)
 import Data.Default (def)
 import Data.Either (Either(..))
-import Data.List (filter, length, (++))
-import Data.Text (Text, append, concat, pack)
+import Data.List (filter, (++), maximum)
+import Data.Text (Text, append, pack)
+import Data.Text.Format (Only(..), Format, format, right, left)
+import Data.Text.Format.Params (Params)
 import Data.Text.IO (putStrLn)
+import Data.Text.Lazy (toStrict)
 import Filesystem.Path (FilePath, (</>), (<.>))
 import System.IO (IO)
 import Text.XML (readFile)
 import Text.Greek.NewTestament.Bible
 import Text.Greek.NewTestament.SBL
+import qualified Data.List as L (length)
+import qualified Data.Text as T (length)
 
 sblgntPath :: FilePath
 sblgntPath = "data" </> "sblgnt-osis" </> "SBLGNT" <.> "osis" <.> "xml"
 
-showBookStats :: Book -> Text
-showBookStats (Book _ t ss) = concat
-  [ t
-  , ": "
-  , report $ \x -> case x of { (SegmentParagraph (Start Paragraph)) -> True ; _ -> False }
-  , " \x00B6s, "
-  , report $ \x -> case x of { (SegmentChapter (Start (Chapter _))) -> True ; _ -> False }
-  , " chs, "
-  , report $ \x -> case x of { (SegmentVerse (Start (Verse _))) -> True ; _ -> False }
-  , " vss, "
-  , report $ \x -> case x of { (SegmentWord _) -> True ; _ -> False }
-  , " words"
-  ]
-  where report f = pack . show . length $ filter f ss
+format' :: Params s => Format -> s -> Text
+format' fmt ps = toStrict . format fmt $ ps
+
+showBookStats :: [Book] -> Book -> Text
+showBookStats bs (Book _ t ss) = format' "{} {} \x00B6s, {} chs, {} vss, {} words" $
+  ( right padTitle ' ' t
+  , padSegments paragraphFilter
+  , padSegments chapterFilter
+  , padSegments verseFilter
+  , padSegments wordFilter
+  )
+  where
+    padTitle = maximum $ T.length . bookTitle <$> bs
+    maxPadSegments f = maximum $ T.length . (\b -> formatSegmentCount (segments b) f) <$> bs
+    padSegments f = left (maxPadSegments f) ' ' $ report f
+    report = formatSegmentCount ss
+    paragraphFilter = \x -> case x of { (SegmentParagraph (Start Paragraph)) -> True ; _ -> False }
+    chapterFilter = \x -> case x of { (SegmentChapter (Start (Chapter _))) -> True ; _ -> False }
+    verseFilter = \x -> case x of { (SegmentVerse (Start (Verse _))) -> True ; _ -> False }
+    wordFilter = \x -> case x of { (SegmentWord _) -> True ; _ -> False }
+    formatSegmentCount ss' f = (format' "{}") (Only . L.length . filter f $ ss')
 
 showResults :: Either SBLError Bible -> [Text]
 showResults (Left b) = [append "Error: " $ pack . show $ b]
 showResults (Right (Bible id title books)) =
   [ id
   , title
-  , append "Books: " (pack . show $ length books)
+  , (format' "Books: {}") (Only . L.length $ books)
   ]
   ++
-  (showBookStats <$> books)
+  (showBookStats books <$> books)
 
 main :: IO ()
 main = do
