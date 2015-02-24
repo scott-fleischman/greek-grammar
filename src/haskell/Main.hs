8944dcb7bd69e3542e6e161ded89f48db1f2f77d
Scott Fleischman <scott.fleischman@logos.com>
2015-02-23 23:24:52 -0800

    Calculate column widths a single time.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -4,9 +4,12 @@ module Main where
 
 import Prelude ((.), ($), Bool(..))
 import Control.Applicative ((<$>))
+import Control.Lens ((^.))
+import Control.Lens.Tuple (_1, _2, _3, _4, _5)
 import Control.Monad (mapM_)
 import Data.Default (def)
 import Data.Either (Either(..))
+import Data.Functor (fmap)
 import Data.List (filter, (++), maximum)
 import Data.Text (Text)
 import Data.Text.Format (Only(..), Shown(..), Format, format, right, left)
@@ -27,24 +30,14 @@ sblgntPath = "data" </> "sblgnt-osis" </> "SBLGNT" <.> "osis" <.> "xml"
 format' :: Params s => Format -> s -> Text
 format' fmt ps = toStrict . format fmt $ ps
 
-showBookStats :: [Book] -> Book -> Text
-showBookStats bs (Book _ t ss) = format' "{} {} \x00B6s, {} chs, {} vss, {} words" $
-  ( right padTitle ' ' t
-  , padSegments paragraphFilter
-  , padSegments chapterFilter
-  , padSegments verseFilter
-  , padSegments wordFilter
-  )
+getBookStats :: Book -> (Text, Text, Text, Text, Text)
+getBookStats (Book _ t ss) = (t, count paragraphs, count chapters, count verses, count words)
   where
-    padTitle = maximum $ T.length . bookTitle <$> bs
-    maxPadSegments f = maximum $ T.length . (\b -> formatSegmentCount (segments b) f) <$> bs
-    padSegments f = left (maxPadSegments f) ' ' $ report f
-    report = formatSegmentCount ss
-    paragraphFilter = \x -> case x of { (SegmentParagraph (Start Paragraph)) -> True ; _ -> False }
-    chapterFilter = \x -> case x of { (SegmentChapter (Start (Chapter _))) -> True ; _ -> False }
-    verseFilter = \x -> case x of { (SegmentVerse (Start (Verse _))) -> True ; _ -> False }
-    wordFilter = \x -> case x of { (SegmentWord _) -> True ; _ -> False }
-    formatSegmentCount ss' f = (format' "{}") (Only . L.length . filter f $ ss')
+    count f = (format' "{}") (Only . L.length . filter f $ ss)
+    paragraphs = \x -> case x of { (SegmentParagraph (Start Paragraph)) -> True ; _ -> False }
+    chapters = \x -> case x of { (SegmentChapter (Start (Chapter _))) -> True ; _ -> False }
+    verses = \x -> case x of { (SegmentVerse (Start (Verse _))) -> True ; _ -> False }
+    words = \x -> case x of { (SegmentWord _) -> True ; _ -> False }
 
 showResults :: Either SBLError Bible -> [Text]
 showResults (Left b) = [format' "Error: {}" (Only $ Shown b)]
@@ -54,7 +47,12 @@ showResults (Right (Bible id title books)) =
   , (format' "Books: {}") (Only . L.length $ books)
   ]
   ++
-  (showBookStats books <$> books)
+  (fmap (\s -> format' "{} {} \x00B6s, {} chs, {} vss, {} words" (t s, leftPad _2 s, leftPad _3 s, leftPad _4 s, leftPad _5 s)) stats)
+  where
+    stats = getBookStats <$> books
+    max g = maximum $ (\s -> T.length $ s ^. g) <$> stats
+    t s = right (maximum $ (\s' -> T.length $ s' ^. _1) <$> stats) ' ' (s ^. _1)
+    leftPad g s = left (max g) ' ' (s ^. g)
 
 main :: IO ()
 main = do
