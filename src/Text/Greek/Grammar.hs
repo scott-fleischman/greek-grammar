23dc99dbc62737b042a00ef05ca5ff0e31bd9865
Scott Fleischman <scott.fleischman@logos.com>
2015-04-07 12:41:35 -0700

    Add a couple extra grammar sources.

--- src/Text/Greek/Grammar.hs
+++ src/Text/Greek/Grammar.hs
@@ -25,3 +25,9 @@ mounceSource = Source "William D. Mounce" "The Morphology of Biblical Greek" 199
 
 mounceCitation :: Text -> Citation
 mounceCitation = Citation mounceSource . Section
+
+brooksSource :: Source
+brooksSource = Source "James A. Brooks, Carlton L. Winbery" "A Morphology of New Testament Greek" 1994
+
+smythSource :: Source
+smythSource = Source "Herbert Weird Smyth, Gordon M. Messing" "Greek Grammar" 1956 -- revised by Messing; Copyright 1920 Smyth; Copyright 1956, renewed 1984
