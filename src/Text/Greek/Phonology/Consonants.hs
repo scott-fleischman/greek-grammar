0358acb4fbb6bf5a30880595e44ea8a437bc612d
Scott Fleischman <scott.fleischman@logos.com>
2015-04-08 08:55:49 -0700

    Add records for phoneme classifications.
    This groups related definitions together.

--- src/Text/Greek/Phonology/Consonants.hs
+++ src/Text/Greek/Phonology/Consonants.hs
@@ -14,7 +14,7 @@ data Fricative = Fricative
 
 data AirFlow =
     Stop
-  | Aspirate (Maybe Fricative) -- Stop + Aspiration
+  | Aspirate (Maybe Fricative) -- mounce 11.17 Stop + Aspiration
   | Liquid (Maybe Fricative)
   | DoubleConsonant
   | Nasal (Maybe Fricative)
@@ -23,78 +23,70 @@ data AirFlow =
 data ConsonantPhoneme = Beta | Gamma | GammaNasal | Digamma | Delta | Zeta | Theta | ConsonantalIota | Kappa | Lambda
   | Mu | Nu | Xi | Pi | Rho | RhoRough | Sigma | Tau | Phi | Chi | Psi | RoughBreathing
 
--- GammaNasal : mounce "11.15" A gamma that is followed immediately by a velar (κ γ χ, including the velar double consonant ξ) is a gamma-nasal.
--- mounce "11.18" ρ Initial ρ always has rough breathing.
+-- GammaNasal : mounce 11.15 A gamma that is followed immediately by a velar (κ γ χ, including the velar double consonant ξ) is a gamma-nasal.
+-- mounce 11.18 ρ Initial ρ always has rough breathing.
 
--- Vocal chords
+allConsonantPhonemes :: [ConsonantPhoneme]
+allConsonantPhonemes = [Beta, Gamma, GammaNasal, Digamma, Delta, Zeta, Theta, ConsonantalIota, Kappa, Lambda,
+  Mu, Nu, Xi, Pi, Rho, RhoRough, Sigma, Tau, Phi, Chi, Psi, RoughBreathing]
 
-voiceless :: Cited [ConsonantPhoneme]
-voiceless = mounce §§ ["11.5", "11.12"] $ [Theta, Kappa, Xi, Pi, Sigma, Tau, Phi, Chi, Psi, RhoRough, RoughBreathing] -- ̔ The rough breathing is also a voiceless fricative
+data ConsonantPhonemeClassification = ConsonantPhonemeClassification
+  { vocalChords :: VocalChordsClassification
+  , partOfMouth :: PartOfMouthClassification
+  , airFlow :: AirFlowClassification
+  }
 
-voiced :: Cited [ConsonantPhoneme]
-voiced = mounce § "11.6" $ [Beta, Gamma, Delta, Zeta, Lambda, Mu, Nu, Rho, ConsonantalIota, Digamma, GammaNasal] -- β, γ, δ, ζ, λ, μ, ν, ρ (except ῥ), ι, ϝ, γ-nasal
+data VocalChordsClassification = VocalChordsClassification
+  { voiceless :: Cited [ConsonantPhoneme]
+  , voiced :: Cited [ConsonantPhoneme]
+  }
 
--- Part of Mouth
+data PartOfMouthClassification = PartOfMouthClassification
+  { labial :: Cited [ConsonantPhoneme]
+  , lingual :: Cited [ConsonantPhoneme]
+  , velar :: Cited [ConsonantPhoneme]
+  , dental :: Cited [ConsonantPhoneme]
+  , gutteral :: Cited [ConsonantPhoneme]
+  }
 
-labial :: Cited [ConsonantPhoneme]
-labial = mounce § "11.9" $ [Pi, Beta, Phi, Mu, Psi, Digamma]
+data AirFlowClassification = AirFlowClassification
+  { stop :: Cited [ConsonantPhoneme]
+  , fricative :: Cited [ConsonantPhoneme]
+  , affricate :: Cited [ConsonantPhoneme]
+  , aspirate :: Cited [ConsonantPhoneme]
+  , liquid :: Cited [ConsonantPhoneme]
+  , nasal :: Cited [ConsonantPhoneme]
+  , sibilant :: Cited [ConsonantPhoneme]
+  }
 
-lingual :: Cited [ConsonantPhoneme]
-lingual = mounce § "11.9a" $ [Lambda, Rho, Nu]
+mounceConsonants :: ConsonantPhonemeClassification
+mounceConsonants = ConsonantPhonemeClassification mounceVocalChords mouncePartOfMouth mounceAirFlow
 
-velar :: Cited [ConsonantPhoneme]
-velar = mounce § "11.10" $ [Kappa, Gamma, Chi, Xi]
+mounceVocalChords :: VocalChordsClassification
+mounceVocalChords = VocalChordsClassification
+  { voiceless = mounce §§ ["11.5", "11.12"] $ [Theta, Kappa, Xi, Pi, Sigma, Tau, Phi, Chi, Psi, RhoRough, RoughBreathing]
+  , voiced = mounce § "11.6" $ [Beta, Gamma, Delta, Zeta, Lambda, Mu, Nu, Rho, ConsonantalIota, Digamma, GammaNasal]
+  }
 
-dental :: Cited [ConsonantPhoneme]
-dental = mounce § "11.11" $ [Tau, Delta, Theta, Zeta]
--- When MBG refers to “dentals,” ζ is not included.
+mouncePartOfMouth :: PartOfMouthClassification
+mouncePartOfMouth = PartOfMouthClassification
+  { labial = mounce § "11.9" $ [Pi, Beta, Phi, Mu, Psi, Digamma]
+  , lingual = mounce § "11.9a" $ [Lambda, Rho, Nu]
+  , velar = mounce § "11.10" $ [Kappa, Gamma, Chi, Xi]
+  , dental = mounce § "11.11" $ [Tau, Delta, Theta, Zeta] -- When MBG refers to “dentals,” ζ is not included.
+  , gutteral = mounce § "11.12" $ [RoughBreathing]
+  }
 
-gutteral :: Cited [ConsonantPhoneme]
-gutteral = mounce § "11.12" $ [RoughBreathing]
-
-
--- Air flow
-
-stop :: Cited [ConsonantPhoneme]
-stop = mounce § "11.14" $ [Beta, Gamma, Delta, Pi, Kappa, Tau]
--- The aspirates (φ χ θ) are sometimes called stops (Square of Stops)
-
-fricative :: Cited [ConsonantPhoneme]
-fricative = mounce § "11.15" $ [GammaNasal, Lambda, Mu, Nu, Rho, Sigma, RoughBreathing]
-{-
-μ nasal fricative (sometimes called a liquid fricative)
-ν nasal fricative (sometimes called a liquid fricative)
--}
-
-affricate :: Cited [ConsonantPhoneme]
-affricate = mounce § "11.16" $ [Psi, Xi, Zeta]
-{-
-We will usually call affricates double consonants, since the term is more widely used.
-ψ labial affricate (which can be formed through the combination of a labial [π β φ] and a sibilant)
-ξ velar affricate (which can be formed through the combination of a velar [κ γ χ] and a sibilant)
-ζ dental affricate (which is a combination of σδ, δσ, or δι).
--}
-
--- §11.16a  Assibilate. An assibilate is the combination of a stop and a sibilant. In Greek, all double consonants are assibilates
--- §11.16b. A π and a σ are pronounced exactly the same as a ψ. The change is merely orthographic, i.e., ψ is a way of writing πσ, βσ, or φσ
-
-aspirate :: Cited [ConsonantPhoneme]
-aspirate = mounce § "11.17" $ [Phi, Theta, Chi, RoughBreathing]
--- An aspirate is the combination of a stop and aspiration.
-
-liquid :: Cited [ConsonantPhoneme]
-liquid = mounce § "11.18" $ [Lambda, Rho]
-
-nasal :: Cited [ConsonantPhoneme]
-nasal = mounce § "11.19" $ [Mu, Nu, GammaNasal]
-
-sibilant :: Cited [ConsonantPhoneme]
-sibilant = mounce § "11.20" $ [Sigma, ConsonantalIota, Digamma]
-{-
-σ The sigma is the only true sibilant in Greek.
-ζ ξ ψ The three double consonants are formed by adding δ (or ι), a velar, or a dental, respectively, to a sigma (§11. 16).
-ι ϝ The semi-consonants also functioned as sibilants before they dropped out of use.
--}
+mounceAirFlow :: AirFlowClassification
+mounceAirFlow = AirFlowClassification
+  { stop = mounce § "11.14" $ [Beta, Gamma, Delta, Pi, Kappa, Tau] -- The aspirates (φ χ θ) are sometimes called stops (Square of Stops)
+  , fricative = mounce § "11.15" $ [GammaNasal, Lambda, Mu, Nu, Rho, Sigma, RoughBreathing] -- μ, ν sometimes called a liquid fricative
+  , affricate = mounce § "11.16" $ [Psi, Xi, Zeta]
+  , aspirate = mounce § "11.17" $ [Phi, Theta, Chi, RoughBreathing]
+  , liquid = mounce § "11.18" $ [Lambda, Rho]
+  , nasal = mounce § "11.19" $ [Mu, Nu, GammaNasal]
+  , sibilant = mounce § "11.20" $ [Sigma, ConsonantalIota, Digamma] -- ζ ξ ψ The three double consonants are formed by adding δ (or ι), a velar, or a dental, respectively, to a sigma (§11. 16).
+  }
 
 -- §11.21 Sonant. A sonant is a consonant that produces a vowel sound either immediately before it or after it in order to aid pronunciation
 
