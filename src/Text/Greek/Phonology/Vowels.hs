db4662093d63b49ffb95a82bdfaaa5a22eaa4cef
Scott Fleischman <scott.fleischman@logos.com>
2015-04-09 15:16:59 -0700

    Add vowel phonemes and initial classification example

--- src/Text/Greek/Phonology/Vowels.hs
+++ src/Text/Greek/Phonology/Vowels.hs
@@ -1,93 +1,41 @@
+module Text.Greek.Phonology.Vowels where
 
-data Vowel = Alpha | Epsilon | Eta | Iota | Omicron | Upsilon | Omega | IotaSubscript 
+data Vowel = Alpha | Epsilon | Eta | Iota | Omicron | Upsilon | Omega
 
-data DiphthongQuality = Genuine Vowel Vowel | Spurious Vowel Vowel | Improper Vowel  
+data Diphthong = Genuine Vowel Vowel | Spurious Vowel Vowel | Improper Vowel  
 
-data VowelLength = Short | Long 
+data VowelLength = Short | Long
 
 data RoundedLip = LipClosed | LipClosedMedium | LipOpen | LipOpenMedium
 
 data TonguePosition = TongueClosed | TongueClosedMedium | TongueOpen | TongueOpenMedium  
 
-data Breathing = Rough | Smooth 
 
-data Diacritic = Circumflex | Diaeresis
+data VowelPhoneme =
+    VowelPhoneme Vowel VowelLength
+  | DiphthongPhoneme Diphthong
 
+alphaShort :: VowelPhoneme
+alphaShort = VowelPhoneme Alpha Short
 
-diphthongs :: [(Vowel, Vowel)]
-diphthongs = [ (Alpha, Iota)
-  , (Epsilon, Iota)
-  , (Omicron, Iota)
-  , (Upsilon, Iota)
-  , (Alpha, Upsilon)
-  , (Epsilon, Upsilon)
-  , (Omicron, Upsilon)
-  , (Eta, Upsilon)
-  , (Alpha, IotaSubscript)
-  , (Eta, IotaSubscript)
-  , (Omega, IotaSubscript)
+alphaLong :: VowelPhoneme
+alphaLong = VowelPhoneme Alpha Long
+
+ouGenuine :: VowelPhoneme
+ouGenuine = DiphthongPhoneme (Genuine Omicron Upsilon)
+
+ouSpurious :: VowelPhoneme
+ouSpurious = DiphthongPhoneme (Spurious Omicron Omicron)
+
+data Contraction = Contraction
+  { target :: VowelPhoneme
+  , first :: VowelPhoneme
+  , second :: VowelPhoneme
+  }
+
+contractions :: [Contraction]
+contractions =
+  [ Contraction alphaLong alphaShort alphaShort
+  , Contraction alphaLong alphaLong alphaShort
+  , Contraction alphaLong alphaShort alphaLong
   ]
-
-allvowelphoneme :: [VowelPhoneme]
-allVowelPhoneme = [Alpha, Epsilon, Eta, Iota, Omicron, Upsilon, Omega, IotaSubscript, (Epsilon, Iota), (Alpha, Iota) 
-, (Omicron, Iota), (Alpha, Upsilon), (Epsilon, Upsilon), (Omicron, Upsilon), (Eta, Upsilon), (Alpha, IotaSubscript) 
-, (Eta, IotaSubscript), (Omega, IotaSubscript)
- ]
-
-data VowelPhonemeClassification = VowelPhonemeClassification
-  { diphthongQuality :: DiphthongQualityClassification
-  , vowellength :: VowelLengthClassification
-  , roundedlip :: RoundedLipClassification
-  , toungueposition :: TounguePositionClassification
-  , breathing :: BreathingClassification
-  , diacritic :: DiacriticClassification
-  }
-
-data DiphthongQualityClassification = DiphthongQualityClassification
-  { genuine :: }
-
-data VocalChordsClassification = VocalChordsClassification
-  { voiceless :: Cited [ConsonantPhoneme]
-  , voiced :: Cited [ConsonantPhoneme]
-  }
-
-data PartOfMouthClassification = PartOfMouthClassification
-  { labial :: Cited [ConsonantPhoneme]
-  , lingual :: Cited [ConsonantPhoneme]
-  , velar :: Cited [ConsonantPhoneme]
-  , dental :: Cited [ConsonantPhoneme]
-  , gutteral :: Cited [ConsonantPhoneme]
-  }
-
-data AirFlowClassification = AirFlowClassification
-  { stop :: Cited [ConsonantPhoneme]
-  , fricative :: Cited [ConsonantPhoneme]
-  , affricate :: Cited [ConsonantPhoneme]
-  , aspirate :: Cited [ConsonantPhoneme]
-  , liquid :: Cited [ConsonantPhoneme]
-  , nasal :: Cited [ConsonantPhoneme]
-  , sibilant :: Cited [ConsonantPhoneme]
-  }
-  smythdiphthongquality :: 
-
-
-
-diphthong a short vowel followed by Iota | Upsilon smyth § "5"
-initial Upsilon always has Rough smyth § "10"
-every intitla Rho has rough Breathing
-medial Rho Rho is written Rho rough Rho smooth
-
-shortvowel shortvowel = long
-short short = diphthong
-
-______________________________
-data DiacriticClassification = DiacriticClassification 
-	{Circumflex :: Cited [VowelPhoneme]
-	, Diaeresis :: Cited [VowelPhoneme]
-	}
-
-data BreathingClassification = BreathingClassification
-	{Rough :: Cited [VowelPhoneme]
-	,Smooth :: Cited [VowelPhoneme]
-	}
-
