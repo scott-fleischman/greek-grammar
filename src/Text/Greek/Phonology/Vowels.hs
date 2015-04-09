28ab5a3bada2742b1a1671629b87cc54b17a93e0
jimmyparks <james.parks@logos.com>
2015-04-09 16:07:04 -0700

    Add ContractionAlpha

--- src/Text/Greek/Phonology/Vowels.hs
+++ src/Text/Greek/Phonology/Vowels.hs
@@ -13,12 +13,16 @@ data VowelPhoneme =
     VowelPhoneme Vowel VowelLength
   | Diphthong Vowel Vowel
   | ImproperDiphthong Vowel
+  | SpuriousDiphthong Vowel Vowel
 
 alpha :: VowelLength -> VowelPhoneme
 alpha len = VowelPhoneme Alpha len
 
 omicron :: VowelPhoneme
 omicron = VowelPhoneme Omicron Short
+
+epsilon :: VowelPhoneme
+epsilon = VowelPhoneme Epsilon Short
 
 data Contraction = Contraction
   { target :: VowelPhoneme
@@ -31,6 +35,11 @@ contractions =
   [ Contraction (alpha Long) (alpha Short) (alpha Short)
   , Contraction (alpha Long) (alpha Long) (alpha Short)
   , Contraction (alpha Long) (alpha Short) (alpha Long)
+  , Contraction (Diphthong Alpha Iota) (alpha Short) (Diphthong Alpha Iota)
+  , Contraction (ImproperDiphthong Alpha) (alpha Short) (ImproperDiphthong Alpha)
+  , Contraction (alpha Long) (alpha Short) epsilon
+  , Contraction (ImproperDiphthong Alpha) (alpha Short) (Diphthong Epsilon Iota)
+  , Contraction (alpha Long) (alpha Short) (SpuriousDiphthong Epsilon Iota)
   , Contraction (Diphthong Omicron Upsilon) omicron omicron
   ]
 
