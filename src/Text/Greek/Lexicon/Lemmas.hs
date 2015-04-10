68cd4a9c7d77fd1aa027abda48590feddb5ecd87
jimmyparks <james.parks@logos.com>
2015-04-10 10:31:18 -0700

    Add Lemma Fixes

--- src/Text/Greek/Lexicon/Lemmas.hs
+++ src/Text/Greek/Lexicon/Lemmas.hs
@@ -13,6 +13,9 @@ data Lemma = Lemma
 mas2D :: PartOfSpeech
 mas2D =  MkNoun (SecondDeclension Masculine)
 
+neut2D :: PartOfSpeech
+neut2D = MkNoun (SecondDeclension Neuter)
+
 theos :: Lemma
 theos = Lemma [θ, ε, σ] mas2D
 
@@ -23,7 +26,7 @@ anthropos :: Lemma
 anthropos = Lemma [ᾰ, ν, θ, ρ, π] mas2D
 
 doron :: Lemma
-doron = Lemma [δ, ω, ρ] mas2D
+doron = Lemma [δ, ω, ρ] neut2D
 
 nous :: Lemma
 nous = Lemma [ν, ο] mas2D
@@ -34,17 +37,11 @@ periplous = Lemma [π, ε, ρ, ῑ, λ, ο] mas2D
 ostous :: Lemma
 ostous = Lemma [ο, σ, τ, ε] mas2D
 
-anthropos :: Lemma
-anthropos = [ᾰ, ν, θ, ρ, ω, π] mas2D
-
 odos :: Lemma
 odos = Lemma [ο, δ] mas2D
 
-doron :: Lemma
-doron = Lemma [δ, ω, ρ]
-
 neos :: Lemma
-neos = Lemma [ν, η, ο]
+neos = Lemma [ν, η, ο] mas2D
 
 
 
