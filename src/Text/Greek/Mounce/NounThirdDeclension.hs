5e805741615fdf3358bd23c03b74ffa86cfb0c1a
Scott Fleischman <scott.fleischman@logos.com>
2015-04-24 12:43:55 -0700

    Combine n-3a and n-3b since endings are the same.

--- src/Text/Greek/Mounce/NounThirdDeclension.hs
+++ src/Text/Greek/Mounce/NounThirdDeclension.hs
@@ -9,8 +9,8 @@ import Text.Greek.Mounce.Quote
 
 thirdDeclensionNouns :: [Cited Noun]
 thirdDeclensionNouns = 
-  [ mounce § "n-3a" $
-    Noun "Stems ending in a labial (π β φ)"
+  [ mounce §§ ["n-3a", "n-3b"] $
+    Noun "Stems ending in a labial (π β φ) or a velar (κ γ χ)"
       [nounCaseEndings|
         ς ες
         ος ων
@@ -19,21 +19,19 @@ thirdDeclensionNouns =
         ς ες
       |]
       []
-      [greekWords| Αἰθίοψ κώνωψ λαῖλαψ μώλωψ σκόλοψ |]
-  , mounce § "n-3b" $
-    Noun "Stems ending in a velar (κ γ χ)"
-      [nounCaseEndings|
-        ς ες
-        ος ων
-        ι σι
-        α ας
-        ς ες
-      |]
-      []
-      [greekWords| 
+      [greekWords|
+        Αἰθίοψ κώνωψ λαῖλαψ μώλωψ σκόλοψ
+
+        Ἄραψ λίψ
+
         ἀλώπηξ ἄνθραξ γυνή δεσμοφύλαξ θώραξ
         κῆρυξ κίλιξ κόραξ ὄρνιξ πίναξ
         πλάξ σάρξ σκώληξ Φῆλιξ Φοῖνιξ
         φοῖνιξ φύλαξ χάραξ χοῖνιξ
+
+        αἴξ ἅρπαξ λάρυγξ μάστιξ πτέρυξ
+        σάλπιγξ φάραγξ φλόξ
+
+        θρίξ σαρδόνυξ ψίξ
       |]
   ]
