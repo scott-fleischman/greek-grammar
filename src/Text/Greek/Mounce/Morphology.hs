5197121d235f4ff878e0c657093b9bdc884360d4
Scott Fleischman <scott.fleischman@logos.com>
2015-05-22 08:50:40 -0700

    Add Foldable instance for NounForms

--- src/Text/Greek/Mounce/Morphology.hs
+++ src/Text/Greek/Mounce/Morphology.hs
@@ -5,6 +5,7 @@ module Text.Greek.Mounce.Morphology where
 
 import Data.Data
 import Data.List
+import Data.Monoid ((<>))
 
 data NounForms a = NounForms
   { nomSg :: a, nomPl :: a
@@ -20,6 +21,9 @@ deriving instance (Show a) => Show (NounForms a)
 
 instance Functor NounForms where
   fmap f (NounForms f0 f1 f2 f3 f4 f5 f6 f7 f8 f9) = NounForms (f f0) (f f1) (f f2) (f f3) (f f4) (f f5) (f f6) (f f7) (f f8) (f f9)
+
+instance Foldable NounForms where
+  foldMap f (NounForms f0 f1 f2 f3 f4 f5 f6 f7 f8 f9) = f f0 <> f f1 <> f f2 <> f f3 <> f f4 <> f f5 <> f f6 <> f f7 <> f f8 <> f f9
 
 data Noun = Noun
   { nounDefinition :: String
