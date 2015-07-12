00fab8ba714f177dd1d523ef2896a7ef7bf26a7e
Scott Fleischman <scott.fleischman@logos.com>
2015-07-11 23:10:37 -0700

    Add start of another model of letters and sounds.

---
+++ haskell/greek-grammar/src/Text/Greek/Script/Letter.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module Text.Greek.Script.Letter where
+
+import Data.Data
+
+data Letter
+  = L_α | L_β | L_γ | L_δ | L_ε | L_ζ | L_η | L_θ | L_ι | L_κ | L_λ | L_μ
+  | L_ν | L_ξ | L_ο | L_π | L_ρ | L_σ | L_τ | L_υ | L_φ | L_χ | L_ψ | L_ω
+  deriving (Eq, Show, Ord, Data, Typeable)
+data LetterCase = Lowercase | Uppercase deriving (Eq, Show, Ord, Data, Typeable)
+data Accent = Acute | Grave | Circumflex deriving (Eq, Show, Ord, Data, Typeable)
+data Breathing = Smooth | Rough deriving (Eq, Show, Ord, Data, Typeable)
+data Subscript_ι = Subscript_ι deriving (Eq, Show, Ord, Data, Typeable)
+data Diaeresis = Diaeresis deriving (Eq, Show, Ord, Data, Typeable)
+data FinalForm = FinalForm deriving (Eq, Show, Ord, Data, Typeable)
+
+data MarkedLetter = MarkedLetter
+  { letter :: Letter
+  , letterCase :: LetterCase
+  , accent :: Maybe Accent
+  , breathing :: Maybe Breathing
+  , subscript_ι :: Maybe Subscript_ι
+  , diaeresis :: Maybe Diaeresis
+  , finalForm :: Maybe FinalForm
+  }
+
+data Consonant
+  = C_β | C_γ | C_δ | C_ζ | C_θ | C_κ | C_λ | C_μ | C_ν
+  | C_ξ | C_π | C_ρ | C_σ | C_τ | C_φ | C_χ | C_ψ | C_rough
+  deriving (Eq, Show, Ord, Data, Typeable)
+data Vowel = V_α | V_ε | V_η | V_ι | V_ο | V_υ | V_ω
+  deriving (Eq, Show, Ord, Data, Typeable)
+data Diphthong = D_αι | D_ει | D_οι | D_αυ | D_ευ | D_ου | D_ηυ | D_ωυ | D_υι
+  deriving (Eq, Show, Ord, Data, Typeable)
+data ImproperDiphthong = I_ᾳ | I_ῃ | I_ῳ
+  deriving (Eq, Show, Ord, Data, Typeable)
+
+data VocalicSound
+  = VowelSound Vowel
+  | DiphthongSound Diphthong
+  | ImproperDiphthongSound ImproperDiphthong
+
+data SoundUnit
+  = ConsonantSoundUnit Consonant
+  | VocalicSoundUnit VocalicSound (Maybe Accent)
+
+makeConsonant :: Letter -> Maybe Consonant
+makeConsonant L_β = Just C_β
+makeConsonant L_γ = Just C_γ
+makeConsonant L_δ = Just C_δ
+makeConsonant L_ζ = Just C_ζ
+makeConsonant L_θ = Just C_θ
+makeConsonant L_κ = Just C_κ
+makeConsonant L_λ = Just C_λ
+makeConsonant L_μ = Just C_μ
+makeConsonant L_ν = Just C_ν
+makeConsonant L_ξ = Just C_ξ
+makeConsonant L_π = Just C_π
+makeConsonant L_ρ = Just C_ρ
+makeConsonant L_σ = Just C_σ
+makeConsonant L_τ = Just C_τ
+makeConsonant L_φ = Just C_φ
+makeConsonant L_χ = Just C_χ
+makeConsonant L_ψ = Just C_ψ
+makeConsonant _ = Nothing
+
+makeVowel :: Letter -> Maybe Vowel
+makeVowel L_α = Just V_α
+makeVowel L_ε = Just V_ε
+makeVowel L_η = Just V_η
+makeVowel L_ι = Just V_ι
+makeVowel L_ο = Just V_ο
+makeVowel L_υ = Just V_υ
+makeVowel L_ω = Just V_ω
+makeVowel _ = Nothing
+
+makeDiphthong :: Vowel -> Vowel -> Maybe Diphthong
+makeDiphthong V_α V_ι = Just D_αι
+makeDiphthong V_ε V_ι = Just D_ει
+makeDiphthong V_ο V_ι = Just D_οι
+makeDiphthong V_α V_υ = Just D_αυ
+makeDiphthong V_ε V_υ = Just D_ευ
+makeDiphthong V_ο V_υ = Just D_ου
+makeDiphthong V_η V_υ = Just D_ηυ
+makeDiphthong V_ω V_υ = Just D_ωυ
+makeDiphthong V_υ V_ι = Just D_υι
+makeDiphthong _ _ = Nothing
+
+makeImproperDiphthong :: Vowel -> Subscript_ι -> Maybe ImproperDiphthong
+makeImproperDiphthong V_α Subscript_ι = Just I_ᾳ
+makeImproperDiphthong V_η Subscript_ι = Just I_ῃ
+makeImproperDiphthong V_ω Subscript_ι = Just I_ῳ
+makeImproperDiphthong _ _ = Nothing
+
+lettersToSounds :: [MarkedLetter] -> [SoundUnit]
+lettersToSounds [] = []
+lettersToSounds (ml1 : ml2 : mls)
+  | Just v1 <- makeVowel (letter ml1)
+  , Just v2 <- makeVowel (letter ml2)
+  , Nothing <- diaeresis ml2
+  , Just d <- makeDiphthong v1 v2
+  , a <- accent ml2
+  = VocalicSoundUnit (DiphthongSound d) a : lettersToSounds mls
+
+lettersToSounds (ml : mls)
+  | Just c <- makeConsonant (letter ml)
+  = ConsonantSoundUnit c : lettersToSounds mls
+
+  | Just v <- makeVowel (letter ml)
+  , Just s <- subscript_ι ml
+  , Just i <- makeImproperDiphthong v s
+  , a <- accent ml
+  = VocalicSoundUnit (ImproperDiphthongSound i) a : lettersToSounds mls
+
+  | Just v <- makeVowel (letter ml)
+  , a <- accent ml
+  = VocalicSoundUnit (VowelSound v) a : lettersToSounds mls
+
+  | True
+  = lettersToSounds mls
