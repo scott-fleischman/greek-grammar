2e7ae54b8eb5cd7d8b2a77f1700fa273cb2a6eab
Scott Fleischman <scott.fleischman@logos.com>
2015-07-20 08:11:30 -0700

    Start sketching code direction.

--- haskell/greek-grammar/src/Text/Greek/NewTestament/SBLSimple.hs
+++ haskell/greek-grammar/src/Text/Greek/NewTestament/SBLSimple.hs
@@ -55,10 +55,45 @@ data Chapter = Chapter Milestone
 data Verse = Verse Milestone
 
 data OsisError
-  = OsisError
+  = InvalidName Name
+  | NotSingleNode [Node]
+  | NotElement Node
 
 documentToOsisText :: Document -> Either OsisError OsisText
 documentToOsisText = handleOsis . documentRoot
 
+singleNode :: [Node] -> Either OsisError Node
+singleNode [n] = Right n
+singleNode ns = Left (NotSingleNode ns)
+
+singleChildNode :: Element -> Either OsisError Node
+singleChildNode (Element name attributes nodes) = singleNode nodes
+
+nodeAsElement :: Node -> Either OsisError Element
+nodeAsElement (NodeElement e) = Right e
+nodeAsElement n = Left . NotElement $ n
+
+osisNamespace :: Text
+osisNamespace = "http://www.bibletechnologies.net/2003/OSIS/namespace"
+
+osisName :: Name -> Text -> Either OsisError Name
+osisName n@(Name actual (Just namespace) _) expected
+  | namespace == osisNamespace
+  , actual == expected
+  = Right n
+  | otherwise
+  = Left (InvalidName n)
+
+osisElementName :: Text -> Element -> Either OsisError Element
+osisElementName expected (Element name attributes nodes) = Element <$> osisName name expected <*> pure attributes <*> pure nodes
+
 handleOsis :: Element -> Either OsisError OsisText
-handleOsis _ = Left OsisError
+handleOsis e
+    = osisElementName "osis" e
+  >>= singleChildNode
+  >>= nodeAsElement
+  >>= handleOsisText
+
+handleOsisText :: Element -> Either OsisError OsisText
+handleOsisText = const . Right . OsisText "" header $ []
+  where header = Header "" "" "" "" "" ""
