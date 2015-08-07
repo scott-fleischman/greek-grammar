cf3dd50cfe8b65806f0597a95c9784501ff857bd
Scott Fleischman <scott.fleischman@logos.com>
2015-08-06 20:14:48 -0700

    Line up columns vertically.

--- haskell/greek-grammar/src/Text/Greek/Mounce/Verb.hs
+++ haskell/greek-grammar/src/Text/Greek/Mounce/Verb.hs
@@ -9,7 +9,7 @@ data Voice = Active | Middle | Passive
 data Mood = Indicative | Subjunctive
 
 data VerbKind a b c d where
-  PresentActiveIndicative :: VerbKind 1 'Present 'Active 'Indicative
-  PresentMiddleIndicative :: VerbKind 1 'Present 'Middle 'Indicative
+  PresentActiveIndicative  :: VerbKind 1 'Present 'Active  'Indicative
+  PresentMiddleIndicative  :: VerbKind 1 'Present 'Middle  'Indicative
   PresentPassiveIndicative :: VerbKind 1 'Present 'Passive 'Indicative
-  Aorist2Indicative :: VerbKind 2 'Aorist 'Active 'Indicative
+  Aorist2Indicative        :: VerbKind 2 'Aorist  'Active  'Indicative
