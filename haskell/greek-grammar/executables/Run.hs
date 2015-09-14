870521354c75f3bc7d44e2b2e1200bfbe488d954
Scott Fleischman <scott.fleischman@logos.com>
2015-09-14 08:25:50 -0700

    Use either so errors don't get silently ignored on run.

--- haskell/greek-grammar/executables/Run.hs
+++ haskell/greek-grammar/executables/Run.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.Maybe
+import Data.Either
 import Text.Greek.Source.All
 import Text.Greek.Script.Raw
 import qualified Data.Text as T
@@ -13,8 +13,15 @@ main :: IO ()
 main = do
   result <- loadAll
   case result of
-    Left es -> mapM_ (T.putStrLn . T.pack . show) es
-    Right xs -> mapM_ (\x -> T.print "{}\n" (T.Only . T.pack . show $ x)) $ 
-      take 10 . catMaybes . fmap (\(Word s r) -> ensureText s r) . concatMap workWords $
-      xs
+    Left es -> printErrors es
+    Right xs -> works xs
+      
+works :: [Work] -> IO ()
+works ws = case errors of
+  _ : _ -> printErrors errors
+  [] -> mapM_ (\x -> T.print "{}\n" (T.Only . T.pack . show $ x)) . take 10 $ results
+  where
+    (errors, results) = partitionEithers . fmap (\(Word s r) -> ensureText s r) . concatMap workWords $ ws
 
+printErrors :: (Show e, Foldable t) => t e -> IO ()
+printErrors = mapM_ (T.putStrLn . T.pack . show)
--- haskell/greek-grammar/src/Text/Greek/Script/Raw.hs
+++ haskell/greek-grammar/src/Text/Greek/Script/Raw.hs
@@ -7,15 +7,22 @@ import Data.Set (Set)
 import Data.Text (Text)
 import Text.Greek.FileReference
 --import Text.Greek.Source.All
+import Text.Greek.Utility
 import qualified Data.Text as T
 
 data Unit = Unit { rawLetter :: Char, rawMarks :: Set Char }
 
-ensureText :: Text -> FileReference -> Maybe [(Char, FileCharReference)]
+data RawError
+  = RawErrorMultipleLines FileReference Text
+  | RawErrorMismatchLength FileReference Text
+  deriving Show
+
+ensureText :: Text -> FileReference -> Either RawError [(Char, FileCharReference)]
 ensureText t r = do
-  (p, l, c1, c2) <- ensureSingleLine r
-  cs <- ensureLengthMatches t c1 c2
+  (p, l, c1, c2) <- onErr RawErrorMultipleLines $ ensureSingleLine r
+  cs <- onErr RawErrorMismatchLength $ ensureLengthMatches t c1 c2
   return $ fmap (_2 %~ FileCharReference p . LineReference l) cs
+    where onErr e = maybeToEither (e r t)
 
 ensureSingleLine :: FileReference -> Maybe (Path, Line, Column, Column)
 ensureSingleLine (FileReference p (LineReference l1 c1) (LineReference l2 c2)) | l1 == l2 = Just (p, l1, c1, c2)
