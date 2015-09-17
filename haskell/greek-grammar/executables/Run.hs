76f917b9d1ccb18112491cf03535051e82adf4eb
Scott Fleischman <scott.fleischman@logos.com>
2015-09-17 12:35:43 -0700

    Parameterize word type.

--- haskell/greek-grammar/executables/Run.hs
+++ haskell/greek-grammar/executables/Run.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 
 module Main where
 
 import Control.Lens
 import Data.Either
 import Data.List
-import Data.Set (Set)
 import Text.Greek.Render
 import Text.Greek.Source.All
 import Text.Greek.Utility
@@ -16,36 +15,45 @@ import qualified Data.Text.Lazy as L
 import qualified Text.Greek.Script.Unit as U
 
 main :: IO ()
-main = do
-  result <- loadAll
-  case result of
-    Left es -> printErrors es
-    Right xs -> works xs
-      
-works :: [Work] -> IO ()
-works ws = case errors of
+main = loadAll >>= handleEither >>= textWorks
+
+textWorks :: [TextWork] -> IO ()
+textWorks ws = case errors of
   _ : _ -> printErrors errors
-  [] -> renderSummary . unitMarkLetterPairs . concat $ results
+  [] -> renderSummary . unitCharLetters . concat $ results
   where
-    (errors, results) = partitionEithers . fmap (\(Word s _) -> U.toUnits s) . concatMap workWords $ ws
+    (errors, results) = partitionEithers . fmap (\(BasicWord s _) -> U.toUnits s) . concatMap workContent $ ws
 
 renderSummary :: (Render b, Ord b, Foldable t) => [(b, t a)] -> IO ()
 renderSummary = renderAll . fmap (over _2 length) . sortOn fst
 
-unitMarkLetterPairs :: (Ord l, Ord m) => [U.Unit l m] -> [((m, l), [U.Unit l m])]
-unitMarkLetterPairs = concatQuery (U.getMarkLetterPairs)
+--unitMarkLetterPairs :: (Ord l, Ord m) => [U.Unit l m] -> [((m, l), [U.Unit l m])]
+--unitMarkLetterPairs = concatQuery (U.getMarkLetterPairs)
 
-unitLetterMarkSets :: (Ord l, Ord m) => [U.Unit l m] -> [((l, Set m), [U.Unit l m])]
-unitLetterMarkSets = query U.getLetterMarkSet
+--unitLetterMarkSets :: (Ord l, Ord m) => [U.Unit l m] -> [((l, Set m), [U.Unit l m])]
+--unitLetterMarkSets = query U.getLetterMarkSet
 
-unitLetters :: Ord l => [U.Unit l m] -> [(l, [U.Unit l m])]
-unitLetters = query U.unitLetter
+unitCharLetters :: [U.UnitChar] -> [(U.LetterChar, [U.UnitChar])]
+unitCharLetters = query U.unitLetter
 
-unitMarks :: Ord m => [U.Unit l m] -> [(m, [U.Unit l m])]
-unitMarks = concatQuery U.getMarks
+unitCharMarks :: [U.UnitChar] -> [(U.MarkChar, [U.UnitChar])]
+unitCharMarks = concatQuery U.getMarks
 
 renderAll :: Render t => [t] -> IO ()
 renderAll = mapM_ (T.putStrLn . L.toStrict . render)
 
-printErrors :: (Show e, Foldable t) => t e -> IO ()
-printErrors = mapM_ (T.putStrLn . T.pack . show)
+printErrors :: (Show e, Foldable t) => t e -> IO a
+printErrors es = do
+  mapM_ (T.putStrLn . T.pack . show) es
+  fail "failure"
+
+handleEither :: (Show e, Foldable t) => Either (t e) x -> IO x
+handleEither (Left es) = printErrors es
+handleEither (Right x) = return x
+
+handleListEither :: (Show e) => [Either e x] -> IO [x]
+handleListEither eithers = case errors of
+  _ : _ -> printErrors errors
+  [] -> return results
+  where
+    (errors, results) = partitionEithers eithers
--- haskell/greek-grammar/src/Text/Greek/Source/All.hs
+++ haskell/greek-grammar/src/Text/Greek/Source/All.hs
@@ -10,33 +10,35 @@ import Text.Greek.Xml.Common
 import Text.Greek.Xml.Parse
 import qualified Text.Greek.Source.Sblgnt as SBL
 
-data Word = Word
-  { wordSurface :: (Text, FileReference)
+type BasicTextWord = BasicWord (Text, FileReference)
+data BasicWord a = BasicWord
+  { wordSurface :: a
   , wordElision :: Maybe (ElisionChar, FileCharReference)
   } deriving Show
 
-data Work = Work
+type TextWork = Work [BasicTextWord]
+data Work a = Work
   { workSource :: WorkSource
   , workTitle :: Text
-  , workWords :: [Word]
+  , workContent :: a
   } deriving Show
 
 data WorkSource = Sblgnt deriving Show
 
-loadAll :: IO (Either [XmlError] [Work])
+loadAll :: IO (Either [XmlError] [TextWork])
 loadAll = loadSblgnt
 
-loadSblgnt :: IO (Either [XmlError] [Work])
+loadSblgnt :: IO (Either [XmlError] [TextWork])
 loadSblgnt = (fmap . fmap) sblgntToWorks $ readParseEvents SBL.sblgntParser sblgntXmlPath
 
-sblgntToWorks :: SBL.Sblgnt -> [Work]
+sblgntToWorks :: SBL.Sblgnt -> [TextWork]
 sblgntToWorks (SBL.Sblgnt _ _ bs) = fmap sblgntBookToWork bs
 
-sblgntBookToWork :: SBL.Book -> Work
+sblgntBookToWork :: SBL.Book -> TextWork
 sblgntBookToWork (SBL.Book _ t ps) = Work Sblgnt t (sblgntParagraphsToWords ps)
 
-sblgntParagraphsToWords :: Foldable t => t SBL.BookParagraph -> [Word]
+sblgntParagraphsToWords :: Foldable t => t SBL.BookParagraph -> [BasicTextWord]
 sblgntParagraphsToWords = fmap sblWordToWord . concatMap (toListOf SBL._ItemWord) . concat . concatMap (toListOf SBL._BookParagraphContent)
 
-sblWordToWord :: SBL.Word -> Word
-sblWordToWord (SBL.Word s e _ _) = Word s e
+sblWordToWord :: SBL.Word -> BasicTextWord
+sblWordToWord (SBL.Word s e _ _) = BasicWord s e
