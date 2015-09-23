1297291caabfe496dda705ca4cded40996818ed6
Scott Fleischman <scott.fleischman@logos.com>
2015-09-23 12:46:13 -0700

    Add stub vocalic syllable.

--- haskell/greek-grammar/src/Text/Greek/Script/Syllable.hs
+++ haskell/greek-grammar/src/Text/Greek/Script/Syllable.hs
@@ -1,5 +1,10 @@
 module Text.Greek.Script.Syllable where
 
-data Vocalic m d
-  = Monophthong m
-  | Diphthong d
+import Text.Greek.FileReference
+import qualified Text.Greek.Script.Letter as Letter
+import qualified Text.Greek.Script.Mark as Mark
+
+data VocalicOpen
+  = OneVowel (Letter.Vowel, FileCharReference) (Maybe (Mark.Diaeresis, FileCharReference))
+  | IotaSubscriptVowel (Letter.Vowel, FileCharReference) (Mark.IotaSubscript, FileCharReference)
+  | TwoVowel (Letter.Vowel, FileCharReference) (Letter.Vowel, FileCharReference)
