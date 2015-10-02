516eb7f48ed0b868f540626dddfdc0850473e82b
Scott Fleischman <scott.fleischman@logos.com>
2015-10-02 07:52:56 -0700

    Install packages.

--- config.js
+++ config.js
@@ -3,11 +3,24 @@ System.config({
   defaultJSExtensions: true,
   transpiler: "traceur",
   paths: {
-    "github:*": "jspm_packages/github/*"
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   },
 
   map: {
+    "bootstrap": "github:twbs/bootstrap@3.3.5",
+    "jquery": "github:components/jquery@2.1.4",
+    "lodash": "npm:lodash@3.10.1",
     "traceur": "github:jmcriffey/bower-traceur@0.0.91",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91"
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery": "github:components/jquery@2.1.4"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
   }
 });
