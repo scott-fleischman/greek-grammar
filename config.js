f7310266bd8209d8bef7f40050d51f89a8432dc6
Scott Fleischman <scott.fleischman@logos.com>
2015-10-02 07:51:59 -0700

    Create a new project configuration.

---
+++ config.js
@@ -0,0 +1,13 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "jspm_packages/github/*"
+  },
+
+  map: {
+    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91"
+  }
+});
