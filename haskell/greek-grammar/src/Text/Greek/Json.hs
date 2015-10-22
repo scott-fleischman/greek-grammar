0046ec78272bdbe266ca854a942633921e3dc639
Scott Fleischman <scott.fleischman@logos.com>
2015-10-22 12:30:23 -0700

    WIP: finish stage0 types.

--- haskell/greek-grammar/src/Text/Greek/Json.hs
+++ haskell/greek-grammar/src/Text/Greek/Json.hs
@@ -15,8 +15,10 @@ import System.FilePath
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Char as Char
+import qualified Data.Foldable as Foldable
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import qualified Data.Text as Text
 import qualified Data.Text.Lazy as Lazy
 import qualified Data.Text.Format as Format
 import qualified Text.Greek.Paths as Path
@@ -175,28 +177,74 @@ makeValueMap xs = Map.fromList indexedList
     uniqueValues = Set.toAscList . Set.fromList $ xs
     indexedList = zip uniqueValues [0..]
 
-makeStage0Types :: [Stage0] -> Maybe [Value]
-makeStage0Types ss = traverse makeInstanceValue . zip [0..] $ ss
+makeStage0Types :: [Stage0] -> [Type]
+makeStage0Types ss = stage0Types
   where
-    workSourceMap = makeValueMap $ fmap (\(x,_,_,_,_) -> x) ss
-    workTitleMap  = makeValueMap $ fmap (\(_,x,_,_,_) -> x) ss
-    stage0WordMap = makeValueMap $ fmap (\(_,_,x,_,_) -> x) ss
-    fileCharMap   = makeValueMap $ fmap (\(_,_,_,x,_) -> x) ss
-    composedMap   = makeValueMap $ fmap (\(_,_,_,_,x) -> x) ss
+    workSourceMap        = makeValueMap $ fmap (\(x,_,_,_,_) -> x) ss
+    workTitleMap         = makeValueMap $ fmap (\(_,x,_,_,_) -> x) ss
+    stage0WordMap        = makeValueMap $ fmap (\(_,_,x,_,_) -> x) ss
+    fileCharReferenceMap = makeValueMap $ fmap (\(_,_,_,x,_) -> x) ss
+    unicodeComposedMap   = makeValueMap $ fmap (\(_,_,_,_,x) -> x) ss
+
+    stage0Types = [workSourceType, workTitleType, stage0WordType, fileCharReferenceType, unicodeComposedType] ++ instanceTypeAsList
+
+    workSourceType = makeSimpleType workSourceName titleWorkSource workSourceMap
+    workTitleType = makeSimpleType workTitleName titleWorkTitle workTitleMap
+    stage0WordType = makeSimpleType stage0WordName titleStage0Word stage0WordMap
+    fileCharReferenceType = makeSimpleType fileCharReferenceName titleFileCharReference fileCharReferenceMap
+    unicodeComposedType = makeSimpleType unicodeComposedName titleUnicodeComposed unicodeComposedMap
+
+    instanceTypeAsList = fmap makeInstanceType . Foldable.toList $ maybeInstanceValues
+
+    makeInstanceType :: [Value] -> Type
+    makeInstanceType = Type "Stage0Instance" "Stage0Instance" [workSourceName, workTitleName, stage0WordName, fileCharReferenceName, unicodeComposedName]
+
+    maybeInstanceValues :: Maybe [Value]
+    maybeInstanceValues = traverse makeInstanceValue . zip [0..] $ ss
 
     makeInstanceValue :: (Int, Stage0) -> Maybe Value
     makeInstanceValue (i, (a, b, c, d, e)) = over _Just (Value i (makeInstanceTitle i e) . Map.fromList) $ sequence
-      [ lookupValueIndex "WorkSource" workSourceMap a
-      , lookupValueIndex "WorkTitle" workTitleMap b
-      , lookupValueIndex "Stage0Word" stage0WordMap c
-      , lookupValueIndex "FileLocation" fileCharMap d
-      , lookupValueIndex "UnicodeComposed" composedMap e
+      [ lookupValueIndex workSourceName workSourceMap a
+      , lookupValueIndex workTitleName workTitleMap b
+      , lookupValueIndex stage0WordName stage0WordMap c
+      , lookupValueIndex fileCharReferenceName fileCharReferenceMap d
+      , lookupValueIndex unicodeComposedName unicodeComposedMap e
       ]
 
     makeInstanceTitle i c = Lazy.toStrict $ Format.format "{} {}" (i, titleUnicodeComposed c)
 
 lookupValueIndex :: Ord a => Text -> Map a Int -> a -> Maybe (Text, Int)
 lookupValueIndex t m v = over _Just (\x -> (t, x)) $ Map.lookup v m
+
+makeSimpleType :: Ord a => Text -> (a -> Text) -> Map a Int -> Type
+makeSimpleType t f = Type t t [] . fmap (\(x,i) -> Value i (f x) Map.empty) . Map.toList
+
+workSourceName :: Text
+workSourceName = "WorkSource"
+
+workTitleName :: Text
+workTitleName = "WorkTitle"
+
+stage0WordName :: Text
+stage0WordName = "Stage0Word"
+
+fileCharReferenceName :: Text
+fileCharReferenceName = "FileLocation"
+
+unicodeComposedName :: Text
+unicodeComposedName = "UnicodeComposed"
+
+titleWorkSource :: All.WorkSource -> Text
+titleWorkSource = Text.pack . show
+
+titleWorkTitle :: All.WorkTitle -> Text
+titleWorkTitle (All.WorkTitle t) = t
+
+titleStage0Word :: Stage0Word -> Text
+titleStage0Word (Stage0Word cs) = Text.pack . fmap (\(Unicode.Composed c) -> c) $ cs
+
+titleFileCharReference :: FileCharReference -> Text
+titleFileCharReference (FileCharReference p (LineReference (Line l) (Column c))) = Lazy.toStrict $ Format.format "{}:{}:{}" (Format.Shown p, l, c)
 
 titleUnicodeComposed :: Unicode.Composed -> Text
 titleUnicodeComposed (Unicode.Composed c) = Lazy.toStrict $ Format.format "U+{} {}" (Format.left 4 '0' . Format.hex . Char.ord $ c, c)
