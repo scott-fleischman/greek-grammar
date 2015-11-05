c8ee1d40405264bd1ad5353872aa581bad22dcb6
Scott Fleischman <scott.fleischman@logos.com>
2015-11-05 12:21:35 -0800

    Separate getView(action) function.

--- html/dev/lib/state.js
+++ html/dev/lib/state.js
@@ -27,16 +27,37 @@ export function applyAction(state = {}, action) {
   const keepProperties = ['index', 'works', 'types'];
   const keep = R.pick(keepProperties, state);
 
-  switch (action.type) {
-    case Action.types.requestIndex: return { ...keep, view: view.loadingIndex };
-    case Action.types.receiveIndex: return { ...keep, view: view.workList, index: action.index };
-    case Action.types.viewWorkList: return { ...keep, view: view.workList };
-    case Action.types.viewTypeList: return { ...keep, view: view.typeList };
-    case Action.types.requestWork: return { ...keep, view: view.loadingWork, workIndex: action.workIndex };
-    case Action.types.receiveWork: return { ...keep, view: view.loadingWork, workIndex: action.workIndex,
-      works: updateMap(state.works, action.workIndex, action.work),
-    };
-    case Action.types.viewWork: return { ...keep, view: view.work, workIndex: action.workIndex };
-    default: return state;
+  const stateUpdate = getStateUpdate(state, action);
+  const view = getView(action.type);
+
+  return { ...keep, ...stateUpdate, view: view };
+}
+
+function getView(actionType) {
+  switch (actionType) {
+    case Action.types.requestIndex: return view.loadingIndex;
+    case Action.types.receiveIndex: return view.workList;
+    case Action.types.viewWorkList: return view.workList;
+    case Action.types.viewTypeList: return view.typeList;
+    case Action.types.requestWork: return view.loadingWork;
+    case Action.types.receiveWork: return view.loadingWork;
+    case Action.types.viewWork: return view.work;
+    default: return undefined;
   }
+}
+
+function getStateUpdate(state, action) {
+  switch (action.type) {
+    case Action.types.receiveIndex: return { index: action.index };
+    case Action.types.requestWork: return { workIndex: action.workIndex };
+    case Action.types.receiveWork: return { workIndex: action.workIndex, works: updateMap(state.works, action.workIndex, action.work) };
+    case Action.types.viewWork: return { workIndex: action.workIndex };
+    default: return {};
+  }
+}
+
+const persistProperties = ['view', 'workIndex'];
+
+function persistAction(action) {
+  return R.compose(R.mergeAll, R.map(R.pick(persistProperties)))([state, action]);
 }
