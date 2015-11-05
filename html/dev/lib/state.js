853e3ff9a4a011dc55cc760721937bae86fc5b75
Scott Fleischman <scott.fleischman@logos.com>
2015-11-05 07:58:27 -0800

    Indicate the properties that are always preserved between state changes.

--- html/dev/lib/state.js
+++ html/dev/lib/state.js
@@ -24,16 +24,19 @@ function updateMap(map, key, value) {
 }
 
 export function applyAction(state = {}, action) {
+  const keepProperties = ['index', 'works', 'types'];
+  const keep = R.pick(keepProperties, state);
+
   switch (action.type) {
-    case Action.types.requestIndex: return { ...state, view: view.loadingIndex };
-    case Action.types.receiveIndex: return { ...state, view: view.workList, index: action.index };
-    case Action.types.viewWorkList: return { ...state, view: view.workList };
-    case Action.types.viewTypeList: return { ...state, view: view.typeList };
-    case Action.types.requestWork: return { ...state, view: view.loadingWork, workIndex: action.workIndex };
-    case Action.types.receiveWork: return { ...state, view: view.loadingWork, workIndex: action.workIndex,
+    case Action.types.requestIndex: return { ...keep, view: view.loadingIndex };
+    case Action.types.receiveIndex: return { ...keep, view: view.workList, index: action.index };
+    case Action.types.viewWorkList: return { ...keep, view: view.workList };
+    case Action.types.viewTypeList: return { ...keep, view: view.typeList };
+    case Action.types.requestWork: return { ...keep, view: view.loadingWork, workIndex: action.workIndex };
+    case Action.types.receiveWork: return { ...keep, view: view.loadingWork, workIndex: action.workIndex,
       works: updateMap(state.works, action.workIndex, action.work),
     };
-    case Action.types.viewWork: return { ...state, view: view.work, workIndex: action.workIndex };
+    case Action.types.viewWork: return { ...keep, view: view.work, workIndex: action.workIndex };
     default: return state;
   }
 }
