11ef319c06924d6500914fa79858329a0466f95f
Scott Fleischman <scott.fleischman@logos.com>
2015-11-07 09:51:18 -0800

    Sketch initial property function.

--- haskell/greek-grammar/src/Text/Greek/Process.hs
+++ haskell/greek-grammar/src/Text/Greek/Process.hs
@@ -1,5 +1,7 @@
 module Text.Greek.Process where
 
+import Data.Text (Text)
+import Text.Greek.FileReference (FileReference)
 import qualified Control.Lens as Lens
 import qualified Text.Greek.Json as Json
 import qualified Text.Greek.Source.All as All
@@ -7,7 +9,31 @@ import qualified Text.Greek.Source.Work as Work
 import qualified Text.Greek.Script.Word as Word
 
 go :: IO ()
-go = All.loadAll >>= handleResult (putStrLn . show . length) . showError
+go = All.loadAll >>= handleResult process . showError
+
+process :: [Work.Indexed [Word.Indexed (Text, FileReference)]] -> IO ()
+process = putStrLn . show . length
+
+getSourceTexts :: [Work.Indexed [Word.Indexed (Text, FileReference)]] -> [(WordLocation, Text)]
+getSourceTexts = concatMap getIndexedWorkProps
+  where
+    getIndexedWorkProps :: Work.Indexed [Word.Indexed (Text, FileReference)] -> [(WordLocation, Text)]
+    getIndexedWorkProps w = fmap (\(i, p) -> ((getWorkIndex w, i), p)) (getWorkProps w)
+
+    getWorkProps :: Work.Indexed [Word.Indexed (Text, FileReference)] -> [(Word.Index, Text)]
+    getWorkProps = fmap getIndexedWordProp . Work.getContent
+
+    getWorkIndex :: Work.Indexed a -> Work.Index
+    getWorkIndex = Lens.view (Work.info . Lens._3)
+
+    getIndexedWordProp :: Word.Indexed (Text, FileReference) -> (Word.Index, Text)
+    getIndexedWordProp w = (getWordIndex w, getWordProp w)
+
+    getWordIndex :: Word.Indexed a -> Word.Index
+    getWordIndex = Lens.view (Word.info . Lens._3)
+
+    getWordProp :: Word.Indexed (Text, FileReference) -> Text
+    getWordProp = Lens.view (Word.surface . Lens._1)
 
 handleResult :: (a -> IO ()) -> Either String a -> IO ()
 handleResult _ (Left e) = putStrLn e
@@ -19,7 +45,4 @@ showError = Lens.over Lens._Left show
 dumpData :: Json.Data -> IO ()
 dumpData = Json.dumpJson
 
-data WordLocation = WordLocation
-  { wordLocationWork :: Work.Index
-  , wordLocationWord :: Word.Index
-  } deriving (Eq, Ord, Show)
+type WordLocation = (Work.Index, Word.Index)
