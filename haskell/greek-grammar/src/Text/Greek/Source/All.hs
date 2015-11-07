17d23b540897754f5a5c64a29d71d9b744f9f4c8
Scott Fleischman <scott.fleischman@logos.com>
2015-11-07 09:10:55 -0800

    Index words on load.

--- haskell/greek-grammar/src/Text/Greek/Source/All.hs
+++ haskell/greek-grammar/src/Text/Greek/Source/All.hs
@@ -2,22 +2,26 @@
 
 module Text.Greek.Source.All where
 
-import Prelude hiding (Word)
-import Control.Lens
-import Data.Foldable
+import Prelude hiding (Word, words)
+import Data.Text (Text)
+import Text.Greek.FileReference (FileReference)
 import Text.Greek.Paths
 import Text.Greek.Xml.Common
 import Text.Greek.Xml.Parse
+import qualified Control.Lens as Lens
 import qualified Text.Greek.Script.Word as Word
 import qualified Text.Greek.Source.Sblgnt as SBL
 import qualified Text.Greek.Source.Work as Work
 
-loadAll :: IO (Either [XmlError] [Work.Indexed [Word.BasicText]])
+loadAll :: IO (Either [XmlError] [Work.Indexed [Word.Indexed (Text, FileReference)]])
 loadAll = do
   sblgntResult <- loadSblgnt
   return $ do
-    sblgntWords <- sblgntResult
-    return $ Work.indexBasic sblgntWords
+    sblgntWorks <- sblgntResult
+    let allWorks = sblgntWorks
+    let indexedWorks = Work.indexBasic allWorks
+    let indexedWorksWords = Lens.over (Lens.each . Work.content) Word.indexBasic indexedWorks
+    return indexedWorksWords
 
 loadSblgnt :: IO (Either [XmlError] [Work.Basic [Word.BasicText]])
 loadSblgnt = (fmap . fmap) sblgntToWorks $ readParseEvents SBL.sblgntParser sblgntXmlPath
@@ -26,19 +30,14 @@ sblgntToWorks :: SBL.Sblgnt -> [Work.Basic [Word.BasicText]]
 sblgntToWorks (SBL.Sblgnt _ _ bs) = fmap sblgntBookToWork bs
 
 sblgntBookToWork :: SBL.Book -> Work.Basic [Word.BasicText]
-sblgntBookToWork (SBL.Book _ t ps) = Work.Work info (concatMap (\(i, x) -> sblgntParagraphToWords i x) . addIndex $ ps)
+sblgntBookToWork (SBL.Book _ t ps) = Work.Work info words
   where
     info = (Work.SourceSblgnt, (Work.Title t))
-    addIndex = zip (fmap Word.ParagraphIndex [0..])
+    addParagraphIndex = zip (fmap Word.ParagraphIndex [0..])
+    words = (concatMap (\(i, x) -> sblgntParagraphToWords i x) . addParagraphIndex $ ps)
 
 sblgntParagraphToWords :: Word.ParagraphIndex -> SBL.BookParagraph -> [Word.BasicText]
-sblgntParagraphToWords i = fmap (sblWordToWord i) . concatMap (toListOf SBL._ItemWord) . concat . toListOf SBL._BookParagraphContent
+sblgntParagraphToWords i = fmap (sblWordToWord i) . concatMap (Lens.toListOf SBL._ItemWord) . concat . Lens.toListOf SBL._BookParagraphContent
 
 sblWordToWord :: Word.ParagraphIndex -> SBL.Word -> Word.BasicText
 sblWordToWord i (SBL.Word s e _ _) = Word.Word (e, i) s
-
-concatSurface :: (Foldable tw, Foldable tc) => Getter s (tc a) -> Work.Basic (tw s) -> [a]
-concatSurface l = concatMap (toList . view l) . (view Work.content)
-
-globalConcatSurface :: (Foldable twork, Foldable tword, Foldable tc) => Getter s (tc a) -> twork (Work.Basic (tword s)) -> [a]
-globalConcatSurface = concatMap . concatSurface
