e6f2c0d7222e88e0b81e2d52d5b334d1743a1a53
Scott Fleischman <scott.fleischman@logos.com>
2015-11-07 10:33:33 -0800

    Generalize getWordSingleType function.

--- haskell/greek-grammar/src/Text/Greek/Process.hs
+++ haskell/greek-grammar/src/Text/Greek/Process.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Text.Greek.Process where
 
 import Data.Text (Text)
@@ -12,27 +14,27 @@ go :: IO ()
 go = All.loadAll >>= handleResult process . showError
 
 process :: [Work.Indexed [Word.IndexedBasic (Text, FileReference)]] -> IO ()
-process = putStrLn . show . length . getGenericSourceTexts getSourceText
+process = putStrLn . show . length . getWordSingleType getSourceText
 
 getSourceText :: Word.IndexedBasic (Text, FileReference) -> Text
 getSourceText = Lens.view (Word.surface . Lens._1)
 
-getGenericSourceTexts :: (Word.IndexedBasic (Text, FileReference) -> Text) -> [Work.Indexed [Word.IndexedBasic (Text, FileReference)]] -> [(WordLocation, Text)]
-getGenericSourceTexts f = concatMap getIndexedWorkProps
+getWordSingleType :: forall a b c. (Word.Indexed a b -> c) -> [Work.Indexed [Word.Indexed a b]] -> [(WordLocation, c)]
+getWordSingleType f = concatMap getIndexedWorkProps
   where
-    getIndexedWorkProps :: Work.Indexed [Word.IndexedBasic (Text, FileReference)] -> [(WordLocation, Text)]
+    getIndexedWorkProps :: Work.Indexed [Word.Indexed a b] -> [(WordLocation, c)]
     getIndexedWorkProps w = fmap (\(i, p) -> ((getWorkIndex w, i), p)) (getWorkProps w)
 
-    getWorkProps :: Work.Indexed [Word.IndexedBasic (Text, FileReference)] -> [(Word.Index, Text)]
+    getWorkProps :: Work.Indexed [Word.Indexed a b] -> [(Word.Index, c)]
     getWorkProps = fmap getIndexedWordProp . Work.getContent
 
-    getWorkIndex :: Work.Indexed a -> Work.Index
+    getWorkIndex :: Work.Indexed x -> Work.Index
     getWorkIndex = Lens.view (Work.info . Lens._1)
 
-    getIndexedWordProp :: Word.IndexedBasic (Text, FileReference) -> (Word.Index, Text)
+    getIndexedWordProp :: Word.Indexed a b -> (Word.Index, c)
     getIndexedWordProp w = (getWordIndex w, f w)
 
-    getWordIndex :: Word.IndexedBasic a -> Word.Index
+    getWordIndex :: Word.Indexed a b -> Word.Index
     getWordIndex = Lens.view (Word.info . Lens._1)
 
 handleResult :: (a -> IO ()) -> Either String a -> IO ()
