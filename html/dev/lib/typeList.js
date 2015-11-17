04c707cffacf5c0a1cc58b5b1d8377c5580a1b37
Scott Fleischman <scott.fleischman@logos.com>
2015-11-17 14:09:13 -0800

    Give hierarchy to the type list.

--- html/dev/lib/typeList.js
+++ html/dev/lib/typeList.js
@@ -2,11 +2,22 @@ import React from 'react';
 import R from 'ramda';
 import { labelNumber } from './labelNumber.js';
 
-const TypeInfo = ({ title, values, url }) => {
+const kindsToClassName = new Map([
+  ['Word Stage', 'typeKindWordStage'],
+  ['Word Stage Transition', 'typeKindWordStageTransition'],
+  ['Word Stage Part Transition', 'typeKindWordStagePartTransition'],
+  ['Word Stage Part', 'typeKindWordStagePart'],
+  ['Word Property', 'typeKindWordProperty'],
+  ['Work Property', 'typeKindWorkProperty'],
+  ['Composite Property', 'typeKindCompositeProperty']
+]);
+
+const TypeInfo = ({ title, kind, values, url }) => {
   const instanceCount = R.compose(R.sum, R.map(x => x.i))(values);
   return (
-    <div>
+    <div className={kindsToClassName.get(kind)}>
       <a href={url}>{title}</a>
+      <span className="typeKind"> &mdash; {kind} &mdash; </span>
       &ensp;
       <span className="typeInfoCounts">{labelNumber(values.length, 'value', 'values')}, {labelNumber(instanceCount, 'instance', 'instances')}</span>
     </div>
