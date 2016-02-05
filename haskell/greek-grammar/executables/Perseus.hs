484372cbcbc11635dab317b06485ae0badb12787
Scott Fleischman <scott.fleischman@logos.com>
2016-02-05 13:03:09 -0800

    Print works with edition count.

--- haskell/greek-grammar/executables/Perseus.hs
+++ haskell/greek-grammar/executables/Perseus.hs
@@ -14,8 +14,16 @@ main = do
   perseusCatalog <- Parse.readParseEvents Inventory.inventoryParser Paths.perseusInventoryXml
   case perseusCatalog of
     Left es -> mapM_ (Text.putStrLn . Text.pack . show) es
-    Right xs -> Text.putStrLn $ Format.format' "groups: {}, Greek works: {}"
-      ( length $ greekCounts
-      , sum $ greekCounts
-      )
-      where greekCounts = filter (/= 0) . fmap (length . filter ((== "grc") . Inventory.workLang) . Inventory.textGroupWorks) . Inventory.inventoryTextGroups $ xs
+    Right inventory -> mapM_ Text.putStrLn greekWorkInfo
+      where
+        greekWorks = getGreekWorks inventory
+        greekWorkInfo = fmap (\x -> Format.format' "Work: {}, Editions: {}" x) greekWorks
+
+getGreekWorks :: Inventory.Inventory -> [(Text.Text, Int)]
+getGreekWorks inventory = presentGreekWorks
+  where
+    textGroups = Inventory.inventoryTextGroups inventory
+    flatWorks = concatMap Inventory.textGroupWorks textGroups
+    greekWorks = filter ((== "grc") . Inventory.workLang) flatWorks
+    greekWorkEditionCount = fmap (\x -> (Inventory.workTitle x, length $ Inventory.workEditions x)) greekWorks
+    presentGreekWorks = filter (\x -> snd x /= 0) greekWorkEditionCount
